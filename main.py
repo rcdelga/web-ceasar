0d4e3fd23a757e7cab06eeb678e0bcb6cbe4e543
rcdelga <triptix@hotmail.com>
2018-09-24 14:39:50 -0500

    Completed: Final formating touches

--- main.py
+++ main.py
@@ -9,29 +9,39 @@ form = """
 <html>
     <head>
       <style>
+          body {{
+              background-color: black;
+          }}
           form {{
-              background-color: #eee;
+              background-color: #777;
               padding: 20px;
               margin: 0 auto;
               width: 540px;
               font: 16px sans-serif;
               border-radius: 10px;
+              font-family: Calibri;
+              font-size: 20px;
           }}
           input {{
-            border-radius: 2px;
+            border-radius: 5px;
           }}
           textarea {{
               margin: 10px 0;
               width: 540px;
               height: 120px;
-              border-radius: 2px;
+              border-radius: 5px;
+          }}
+          #rot {{
+            width: 35px;
+            text-align: center;
           }}
       </style>
     </head>
     <body>
       <form method="POST">
         <label for="rot">Rotate by:</label>
-        <input name="rot" type="text" value="0"><br>
+        <input id="rot" name="rot" type="text" value="0"><br><br>
+        <label for="text">Text to be encrypted:</label><br>
         <textarea type="text" name="text">{0}</textarea><br>
         <input type="submit">
       </form>
